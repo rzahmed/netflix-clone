13cd1f4d8f99cc57d97a10902db5ad3501bf6703
razaahmed0 <rzahmed@ucdavis.edu>
2021-07-24 18:21:32 -0700

    seperate directory for components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Nav from "./Nav";
-import Banner from "./Banner";
-import Row from "./Row";
+import Nav from "./components/Nav";
+import Banner from "./components/Banner";
+import Row from "./components/Row";
 import requests from "./requests";
 
 function App() {
@@ -16,10 +16,10 @@ function App() {
 			/>
 			<Row title="Trending Now" fetchUrl={requests.fetchTrending} />
 			<Row title="Top Rated" fetchUrl={requests.fetchTrending} />
-			<Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-			<Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-			<Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-			<Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+			<Row title="Action " fetchUrl={requests.fetchActionMovies} />
+			<Row title="Comedy " fetchUrl={requests.fetchComedyMovies} />
+			<Row title="Horror " fetchUrl={requests.fetchHorrorMovies} />
+			<Row title="Romance " fetchUrl={requests.fetchRomanceMovies} />
 			<Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 		</div>
 	);
